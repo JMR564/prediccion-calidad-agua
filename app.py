dd28def834bf01b66c08aa2056fdf263dcf03cc6
JMR564 <juanmanuelra5@gmail.com>
2025-05-18 19:10:26 -0500

    Actualización de app.py con nuevas variables

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import numpy as np
-import os
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
-
 
 app = Flask(__name__)
 
@@ -15,34 +8,31 @@ app = Flask(__name__)
 def formulario():
     return render_template("formulario.html")
 
+# Carga inicial del dataset
 df = pd.read_csv("datos_calidad_agua.csv")
 
 @app.route('/resultado', methods=['POST'])
 def resultado():
     ciudad = request.form['ciudad']
     
-    # Leer el CSV
-    df = pd.read_csv("datos_calidad_agua.csv")
-
     # Filtrar por ciudad
     datos = df[df['Municipio'] == ciudad]
 
     if datos.empty:
         return render_template('resultado.html', resultado="Ciudad no encontrada", colores={})
 
-    # Separar variables de entrada y salida
-    X = df[['pH', 'Turbiedad', 'Color', 'Coliformes']]
-    y = df['Nivel']
+    # Variables predictoras y variable objetivo
+    X = df[['pH', 'Turbiedad', 'Color', 'Coliformes_Totales', 'Coliformes_Fecales']]
+    y = df['Nivel de riesgo']
 
-    # Entrenar modelo (Gradient Boosting por ejemplo)
-    from sklearn.ensemble import GradientBoostingClassifier
+    # Entrenar el modelo
     modelo = GradientBoostingClassifier()
     modelo.fit(X, y)
 
     # Datos de entrada para predicción
-    X_pred = datos[['pH', 'Turbiedad', 'Color', 'Coliformes']]
+    X_pred = datos[['pH', 'Turbiedad', 'Color', 'Coliformes_Totales', 'Coliformes_Fecales']]
 
-    # Realizar predicción
+    # Predicción
     prediccion_proba = modelo.predict_proba(X_pred)[0]
     prediccion = modelo.predict(X_pred)[0]
 
@@ -58,4 +48,3 @@ def resultado():
     }
 
     return render_template('resultado.html', resultado=resultado, prediccion_final=prediccion, colores=colores)
-
